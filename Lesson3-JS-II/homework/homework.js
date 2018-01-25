5cc4e51e7bab43e29c42abeb427264c57e9b4b66
Steve Vega <Stevefreake@gmail.com>
2018-01-25 16:15:27 -0700

    completed homework, homework.js

--- Lesson3-JS-II/homework/homework.js
+++ Lesson3-JS-II/homework/homework.js
@@ -3,6 +3,11 @@
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  if (x > y) {
+    return x;
+  } else {
+    return y;
+  }
 }
 
 function greeting(language) {
@@ -11,16 +16,26 @@ function greeting(language) {
   // language: 'English' -> 'Hello!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
+  if (language === 'German') {
+    return 'Guten Tag!';
+  } else if (language === 'English') {
+    return 'Hello!';
+  } else if (language === 'Spanish') {
+    return 'Hola!';
+  } else {
+    return 'Hello!';
+  }
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  return (num === 10 || num === 5);
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
-}
+  return (num < 50 && num > 20)
 
 function isInteger(num) {
   // return true if num is an integer
@@ -29,6 +44,7 @@ function isInteger(num) {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  return Math.floor(num); 
 }
 
 function fizzBuzz(num) {
@@ -36,6 +52,15 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if (num % 3 === 0 && num % 5 === 0){
+    return 'fizzbuzz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  } else {
+    return num;
+  }
 }
 
 function isPrime(num) {
@@ -44,35 +69,42 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  return num / num === 1;
 }
 
 function returnFirst(arr) {
   // return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
   // return the last item of the array
+  return (arr[arr.length - 1]);
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
+  return arr.length + 1;
 }
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
   // return the array
+  return arr.push[item];
 }
 
 function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  return arr.unshift[item];
 }
 
 function wordsToSentence(words) {
@@ -80,26 +112,49 @@ function wordsToSentence(words) {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+  return words.join(' ');
 }
 
 function contains(arr, item) {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  return arr.indexOf(item);
 }
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  totlength = numbers.length;
+  numfin = 0;
+  for (let i = 0; i < totlength - 1; i++){
+    const totnum = numbers[0] += numbers[numbers.length - 1];
+    numbers.pop();
+    numfin = totnum;
+  }
+  return numfin;
 }
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  totlength = numbers.length;
+  numfin = 0;
+  for (let i = 0; i < totlength - 1; i++){
+    const totnum = numbers[0] += numbers[numbers.length - 1];
+    numbers.pop();
+    numfin = totnum;
+  }
+  return (totlength / totnum) * 100;
 }
+
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+  numbers.sort(function(a, b) {
+    return a - b;
+  });
+  return numbers[numbers.length - 1];
 }
 
 // Do not modify code below this line.
